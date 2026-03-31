c43aa2cabe54633060ed61415acb28b602972d6b
faiqzzz <ridhofaiqzzz@gmail.com>
2026-03-31 15:53:46 +0800

    Latihan 2 (Update SKS)

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,12 +1,13 @@
-﻿private void btnHitungDosen_Click(object sender, EventArgs e)
+﻿private void btnUpdateSKS_Click(object sender, EventArgs e)
 {
     try
     {
         Koneksi();
         if (conn.State == ConnectionState.Closed) conn.Open();
-        cmd = new SqlCommand("SELECT COUNT(*) FROM Dosen", conn);
-        int jumlah = Convert.ToInt32(cmd.ExecuteScalar());
-        txtHasil.Text = "Jumlah Dosen: " + jumlah.ToString();
+        string query = "UPDATE MataKuliah SET SKS = 4 WHERE KodeMK = 'IF210101'";
+        cmd = new SqlCommand(query, conn);
+        int hasil = cmd.ExecuteNonQuery();
+        MessageBox.Show("SKS berhasil diupdate! Baris: " + hasil);
     }
     catch (Exception ex) { MessageBox.Show(ex.Message); }
     finally { conn.Close(); }
