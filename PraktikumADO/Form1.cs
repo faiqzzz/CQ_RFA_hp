ad1f9ea07f307035359e9a161c749be3d3992b36
faiqzzz <ridhofaiqzzz@gmail.com>
2026-03-31 16:01:00 +0800

    Validasi Sebelum Update/Insert

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,2 +1,10 @@
-﻿}
+﻿tnUpdate_Click(object sender, EventArgs e)
+{
+    // Cek dulu apakah textbox hasil sudah ada isinya atau belum (contoh validasi simpel)
+    if (string.IsNullOrEmpty(txtHasil.Text))
+    {
+        MessageBox.Show("Hasil harus dihitung dulu sebelum update!");
+        return;
+    }
+    // Lanjut ke query update...
 }
