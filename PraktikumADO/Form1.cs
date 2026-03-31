5d8dd015a8c711df3ec51f5d72ebcd27c118f1fc
faiqzzz <ridhofaiqzzz@gmail.com>
2026-03-31 15:50:58 +0800

    Variabel Global

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,5 +1,4 @@
-﻿private void Koneksi()
-{
-    // Pastikan pakai @ agar backslash terbaca benar
-    conn = new SqlConnection(@"Data Source=DESKTOP-DDDRHRS\RIDHOFAIQAHMAD;Initial Catalog=DBAkademiADO;Integrated Security=True");
-}
+﻿using System.Data.SqlClient;
+
+SqlConnection conn;
+SqlCommand cmd;
