3eb7597592d64687a7a9166dbf03812da4687a0f
faiqzzz <ridhofaiqzzz@gmail.com>
2026-03-31 15:54:30 +0800

    Validasi State Koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -1,14 +1,1 @@
-﻿private void btnInsertProdi_Click(object sender, EventArgs e)
-{
-    try
-    {
-        Koneksi();
-        if (conn.State == ConnectionState.Closed) conn.Open();
-        string query = "INSERT INTO ProgramStudi VALUES ('MI01','Manajemen Informatika')";
-        cmd = new SqlCommand(query, conn);
-        cmd.ExecuteNonQuery();
-        MessageBox.Show("Data Prodi berhasil ditambahkan!");
-    }
-    catch (Exception ex) { MessageBox.Show(ex.Message); }
-    finally { conn.Close(); }
-}
+﻿if (conn.State == ConnectionState.Closed) conn.Open();
